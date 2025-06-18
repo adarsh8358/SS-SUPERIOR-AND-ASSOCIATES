1b9183a22017b11e35e7f4d4b2a3d1fa4f669a6e
adarsh8358 <adarshkushwaha8358@gmail.com>
2025-06-18 08:34:33 +0530

    commit

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => {
                         <li><Link to="/services">Services</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
                         <li><Link to="/about">About Us</Link></li>
-                        <li><Link to="/Help">Help</Link></li>
+                        <li><Link to="/help">Help</Link></li>
                     </ul>
                 </div>
 
