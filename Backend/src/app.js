95931e167af353e05424e9129db458847e82435a
adarsh8358 <adarshkushwaha8358@gmail.com>
2025-06-26 10:25:20 +0530

    test 4

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,17 +7,29 @@ import helpRoutes from './routes/help.routes.js';
 const app = express();
 
 
+// app.use(cors({
+//   origin: ['http://localhost:3000',
+//     'http://localhost:5173',
+//     'https://ss-superior-and-associates-frontend.onrender.com',
+//     'https://sssuperiorassociates.com/',
+//     'https://www.sssuperiorassociates.com/',
+//     "sssuperiorassociates.com",
+//     "www.sssuperiorassociates.com"],
+
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: ['http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
     'http://localhost:5173',
     'https://ss-superior-and-associates-frontend.onrender.com',
-    'https://sssuperiorassociates.com/',
-    'https://www.sssuperiorassociates.com/',
-    "sssuperiorassociates.com",
-    "www.sssuperiorassociates.com"],
-
+    'https://sssuperiorassociates.com',
+    'https://www.sssuperiorassociates.com'
+  ],
   credentials: true
 }));
+
 
 app.use(express.json());
 
